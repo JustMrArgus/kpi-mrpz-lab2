293a5ebf2f6e3aae5d3064060ee4635a031750bd
JustMrArgus <argusprogramming@protonmail.com>
2025-04-07 01:48:14 +0000

    Refactor ArrayList methods
    To make task more correct was made decision to remade
    class methods so now it implements it's own logic
    and do not use bult-in methods of python array

--- array_list.py
+++ array_list.py
@@ -1,56 +1,107 @@
 class ArrayList:
   def __init__(self):
     self.data = []
+    self.size = 0
   
   def length(self) -> int:
-    return len(self.data)
+    return self.size
   
   def append(self, element: str) -> None:
-    self.data.append(element)
+    if self.size == len(self.data):
+      new_array = [None] * (max(1, self.size * 2))
+      for i in range(self.size):
+        new_array[i] = self.data[i]
+      self.data = new_array
+    
+    self.data[self.size] = element
+    self.size += 1
   
   def insert(self, element: str, index: int) -> None:
-    if index < 0 or index > len(self.data):
+    if index < 0 or index > self.size:
       raise IndexError("Index out of range")
-    self.data.insert(index, element)
+    
+    if self.size == len(self.data):
+      new_array = [None] * (max(1, self.size * 2))
+      for i in range(self.size):
+        new_array[i] = self.data[i]
+      self.data = new_array
+  
+    for i in range(self.size, index, -1):
+      self.data[i] = self.data[i-1]
+    
+    self.data[index] = element
+    self.size += 1
   
   def delete(self, index: int) -> str:
-    if index < 0 or index >= len(self.data):
+    if index < 0 or index >= self.size:
       raise IndexError("Index out of range")
-    return self.data.pop(index)
+    
+    removed_element = self.data[index]
+    
+    for i in range(index, self.size - 1):
+      self.data[i] = self.data[i + 1]
+    
+    self.data[self.size - 1] = None
+    self.size -= 1
+    
+    return removed_element
   
   def deleteAll(self, element: str) -> None:
-    self.data = [item for item in self.data if item != element]
+    write_index = 0
+    
+    for i in range(self.size):
+      if self.data[i] != element:
+        self.data[write_index] = self.data[i]
+        write_index += 1
+    
+    for i in range(write_index, self.size):
+      self.data[i] = None
+    
+    self.size = write_index
   
   def get(self, index: int) -> str:
-    if index < 0 or index >= len(self.data):
+    if index < 0 or index >= self.size:
       raise IndexError("Index out of range")
+    
     return self.data[index]
   
   def clone(self):
     new_list = ArrayList()
-    new_list.data = self.data.copy()
+    
+    new_list.data = [None] * max(1, self.size)
+    
+    for i in range(self.size):
+      new_list.data[i] = self.data[i]
+    
+    new_list.size = self.size
     return new_list
   
   def reverse(self) -> None:
-    self.data.reverse()
+    for i in range(self.size // 2):
+      temp = self.data[i]
+      self.data[i] = self.data[self.size - 1 - i]
+      self.data[self.size - 1 - i] = temp
   
   def findFirst(self, element: str) -> int:
-    try:
-      return self.data.index(element)
-    except ValueError:
-      return -1
+    for i in range(self.size):
+      if self.data[i] == element:
+        return i
+    return -1
   
   def findLast(self, element: str) -> int:
-    for i in range(len(self.data) - 1, -1, -1):
+    for i in range(self.size - 1, -1, -1):
       if self.data[i] == element:
         return i
     return -1
   
   def clear(self) -> None:
-    self.data = []
+    for i in range(self.size):
+      self.data[i] = None
+    self.size = 0
   
   def extend(self, elements) -> None:
-    self.data.extend(elements.data)
+    for i in range(elements.size):
+      self.append(elements.data[i])
   
   def __str__(self):
-    return str(self.data)
+    return str(self.data[:self.size])
