6387950fb59811e5faa8d1566fdb29e78eda4f4b
JustMrArgus <argusprogramming@protonmail.com>
2025-04-07 00:47:39 +0000

    Add append method

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -12,3 +12,14 @@ class DoublyLinkedList:
   
   def length(self) -> int:
     return self._size
+  
+  def append(self, element: str) -> None:
+    new_node = Node(element)
+    if self.head is None:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      new_node.prev = self.tail
+      self.tail.next = new_node
+      self.tail = new_node
+      self._size += 1
