25218fb7398a9a728b5668a8192ad07f5c52c148
JustMrArgus <argusprogramming@protonmail.com>
2025-04-07 00:52:31 +0000

    Add insert method

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -23,3 +23,33 @@ class DoublyLinkedList:
       self.tail.next = new_node
       self.tail = new_node
       self._size += 1
+    
+  def insert(self, element: str, index: int) -> None:
+    if index < 0 or index > self._size:
+      raise IndexError("Index out of range")
+        
+    new_node = Node(element)
+        
+    if index == 0:
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+    elif index == self._size:
+        new_node.prev = self.tail
+        self.tail.next = new_node
+        self.tail = new_node
+    else:
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        
+        new_node.prev = current.prev
+        new_node.next = current
+        current.prev.next = new_node
+        current.prev = new_node
+    
+    self._size += 1
