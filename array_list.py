5ba17f40fb114dd84cfcc397da40e6243ab9dedf
JustMrArgus <argusprogramming@protonmail.com>
2025-04-07 00:35:12 +0000

    Add insert method

--- array_list.py
+++ array_list.py
@@ -7,3 +7,8 @@ class ArrayList:
   
   def append(self, element: str) -> None:
     self.data.append(element)
+  
+  def insert(self, element: str, index: int) -> None:
+    if index < 0 or index > len(self.data):
+      raise IndexError("Index out of range")
+    self.data.insert(index, element)
